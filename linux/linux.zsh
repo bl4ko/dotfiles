9a94267e2f71fa96106d5f506b9935d444047c8a
bl4ko <go3969@student.uni-lj.si>
2023-02-24 15:56:06 +0100

    Fixing command not found on linux...

--- linux/linux.zsh
+++ linux/linux.zsh
@@ -2,10 +2,4 @@
 # https://packages.ubuntu.com/search?keywords=command-not-found
 if [ -f /etc/zsh_command_not_found ]; then
   . /etc/zsh_command_not_found
-else
-  echo "command-not-found is missing..."
-  # echo "installing command-not-found..."
-  # sudo apt update && sudo apt install command-not-found
-  #. /etc/zsh_command_not_found
 fi
-
