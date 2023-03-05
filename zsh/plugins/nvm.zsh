94d1b29998d6a3a06d0c4034e7e461a6a2528852
bl4ko <go3969@student.uni-lj.si>
2023-03-05 11:33:47 +0100

    temp fix for nvm lazy loading

--- zsh/plugins/nvm.zsh
+++ zsh/plugins/nvm.zsh
@@ -1,10 +1,9 @@
-# export PATH="$HOME/.nvm/versions/node/v16.19.1/:$PATH"
+export PATH="$HOME/.nvm/versions/node/v16.19.1/bin:$PATH" # Temporary fix for copilot in neovim (lazyloading breaks not found in path)
 
 lazynvm() {
   if test -f "$HOME/.nvm/nvm.sh"; then
     unset -f nvm node npm
     export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   else
     echo "$HOME/.nvm/nvm.sh is missing..." >&2
@@ -26,4 +25,3 @@ npm() {
   lazynvm
   npm "$@"
 }
-
