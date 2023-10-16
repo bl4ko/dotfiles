31c2cd82eba3053b5952e334f426dbe260f8a88a
bl4ko <go3969@student.uni-lj.si>
2023-10-16 19:19:38 +0200

    Fix typo in export node path

--- zsh/plugins/nvm.zsh
+++ zsh/plugins/nvm.zsh
@@ -1,7 +1,7 @@
 # When app tries to use node and it is not lazy loaded by user, it will give 
 # command not found error. To solve this we manually add node to the path.
 # After first use of node, nvm will be loaded and we can use it normally.
-export PATH="$$HOME/.nvm/versions/node/$(ls $HOME/.nvm/versions/node | sort -r | head -n 1)/bin:$PATH" 
+export PATH="$HOME/.nvm/versions/node/$(ls $HOME/.nvm/versions/node | sort -r | head -n 1)/bin:$PATH" 
 
 lazynvm() {
   if test -f "$HOME/.nvm/nvm.sh"; then
