1d0a0f58ede8f9c1dafb618db9de382d246eec68
bl4ko <go3969@student.uni-lj.si>
2023-11-07 21:49:16 +0100

    Updating nvm script

--- zsh/init-scripts/nvm.sh
+++ zsh/init-scripts/nvm.sh
@@ -11,5 +11,8 @@ install_nvm () {
     git switch -c `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
     ) && source "$NVM_DIR/nvm.sh"    
     echo -e "${TICK} NVM installed!"
+    echo -e "${INFO} Installing latest node..."
+    if ! nvm install node; then echo -e "${CROSS} Failed installing node"; exit 1; fi
+    echo -e "${TICK} Node installed!"
   fi
 }
